4e83671d3a85c378ee5c267d1ccd2cb4c8d3b8ba
ali kalantari <clash.iranali@gmail.com>
2021-09-25 18:18:17 +0330

    handle active class of <li> tag in panel.js

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./panel.css";
 import { FaBook } from "react-icons/fa";
 import { GrPersonalComputer } from "react-icons/gr";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Panel(props) {
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+
   return (
-    <div className={props.show ? 'panel show' : 'panel hide'}>
+    <div className={props.show ? "panel show" : "panel hide"}>
       <div className="panel-top">
         <div className="panel-title">
           <span id="panel-title">نام سایت</span>
@@ -17,24 +21,85 @@ export default function Panel(props) {
       </div>
       <div className="panel-bottom">
         <div className="tab-content">
-            <div className='panel-tabs'>
-                <span className='tabs-title'>کلاس ها</span>
-                <ul>
-                    <Link to='/dashboard'><li className='tabs-item active'>داشبورد</li></Link>
-                    <Link to='/course'><li className='tabs-item'>کلاس ها</li></Link>
-                    <li className='tabs-item'>آزمون های پیش رو</li>
-                    <li className='tabs-item '>ارزیابی ها</li>
-                    <li className='tabs-item'> جلسات آنلاین پیش رو</li>
-                    <li className='tabs-item'>مدارک</li>
-                    <li className='tabs-item'>راهنما</li>
-                </ul>
-            </div>
+          <div className="panel-tabs">
+            <span className="tabs-title">کلاس ها</span>
+            <ul>
+              <Link to="/dashboard">
+                <li
+                  className={
+                    splitLocation[1] === "dashboard"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  داشبورد
+                </li>
+              </Link>
+              <Link to="/course">
+                <li
+                  className={
+                    splitLocation[1] === "course"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  کلاس ها
+                </li>
+              </Link>
+              <li
+                className={
+                  splitLocation[1] === "match"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                آزمون های پیش رو
+              </li>
+              <li
+                className={
+                  splitLocation[1] === "evaluation"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                ارزیابی ها
+              </li>
+              <li
+                className={
+                  splitLocation[1] === "onlineclass"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                {" "}
+                جلسات آنلاین پیش رو
+              </li>
+              <li
+                className={
+                  splitLocation[1] === "userdoc"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                مدارک
+              </li>
+              <li
+                className={
+                  splitLocation[1] === "alalalalal"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                راهنما
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className="sidebar-wrapper">
           <div className="sidebar active">
             <div className="sidebar-item icon">
-              <GrPersonalComputer color='white' />
+              <GrPersonalComputer color="white" />
             </div>
           </div>
 
@@ -43,7 +108,6 @@ export default function Panel(props) {
               <GrPersonalComputer className="sidebar-icon" />
             </div>
           </div> */}
-          
         </div>
       </div>
     </div>
