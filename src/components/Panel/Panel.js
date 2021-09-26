795213760d7cdc438132803d556c17de72e0f617
ali kalantari <clash.iranali@gmail.com>
2021-09-26 15:03:08 +0330

    add second list to panel component

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./panel.css";
 import { FaBook } from "react-icons/fa";
+import { BiBriefcase } from "react-icons/bi";
 import { GrPersonalComputer } from "react-icons/gr";
 import { Link, useLocation } from "react-router-dom";
 
@@ -8,6 +9,19 @@ export default function Panel(props) {
   const location = useLocation();
   const { pathname } = location;
   const splitLocation = pathname.split("/");
+
+  const [pc, setpc] = useState(true);
+  const [face, setface] = useState(false);
+  const pctoggle = () => {
+    setpc(true);
+    setface(false);
+    // console.log(value)
+  };
+  const facetoggle = () => {
+    setpc(false);
+    setface(true);
+    // console.log(value)
+  };
 
   return (
     <div className={props.show ? "panel show" : "panel hide"}>
@@ -21,13 +35,14 @@ export default function Panel(props) {
       </div>
       <div className="panel-bottom">
         <div className="tab-content">
-          <div className="panel-tabs">
+          <div className={pc ? "panel-tabs active" : "panel-tabs"}>
             <span className="tabs-title">کلاس ها</span>
             <ul>
               <Link to="/dashboard">
                 <li
                   className={
-                    splitLocation[1] === "dashboard" || "changepassword"
+                    splitLocation[1] === "dashboard" ||
+                    splitLocation[1] === "changepassword"
                       ? "tabs-item active"
                       : "tabs-item"
                   }
@@ -64,28 +79,28 @@ export default function Panel(props) {
               >
                 ارزیابی ها
               </li>
-              <Link to='/onlineclass'>
-              <li
-                className={
-                  splitLocation[1] === "onlineclass"
-                    ? "tabs-item active"
-                    : "tabs-item"
-                }
-              >
-                {" "}
-                جلسات آنلاین پیش رو
-              </li>
+              <Link to="/onlineclass">
+                <li
+                  className={
+                    splitLocation[1] === "onlineclass"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  {" "}
+                  جلسات آنلاین پیش رو
+                </li>
               </Link>
-              <Link to='/userdoc'>
-              <li
-                className={
-                  splitLocation[1] === "userdoc"
-                    ? "tabs-item active"
-                    : "tabs-item"
-                }
-              >
-                مدارک
-              </li>
+              <Link to="/userdoc">
+                <li
+                  className={
+                    splitLocation[1] === "userdoc"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  مدارک
+                </li>
               </Link>
               <li
                 className={
@@ -98,20 +113,105 @@ export default function Panel(props) {
               </li>
             </ul>
           </div>
+          {/*
+           */}
+          {/*
+           */}
+          {/*
+           */}
+          <div className={face ? "panel-tabs active" : "panel-tabs"}>
+            <span className="tabs-title">تکالیف</span>
+            <ul>
+              <Link to="/userdoc">
+                <li
+                  className={
+                    splitLocation[1] === "userdoc"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  مدارک
+                </li>
+              </Link>
+
+              <li
+                className={
+                  splitLocation[1] === "match"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                آزمون های پیش رو
+              </li>
+              <Link to="/course">
+                <li
+                  className={
+                    splitLocation[1] === "course"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  کلاس ها
+                </li>
+              </Link>
+              <li
+                className={
+                  splitLocation[1] === "evaluation"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                 تکالیف فعال
+              </li>
+              <Link to="/dashboard">
+                <li
+                  className={
+                    splitLocation[1] === "dashboard" ||
+                    splitLocation[1] === "changepassword"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  داشبورد
+                </li>
+              </Link>
+              <li
+                className={
+                  splitLocation[1] === "alalalalal"
+                    ? "tabs-item active"
+                    : "tabs-item"
+                }
+              >
+                لیست نمرات
+              </li>
+              <Link to="/onlineclass">
+                <li
+                  className={
+                    splitLocation[1] === "onlineclass"
+                      ? "tabs-item active"
+                      : "tabs-item"
+                  }
+                >
+                  {" "}
+                  جلسات آنلاین پیش رو
+                </li>
+              </Link>
+            </ul>
+          </div>
         </div>
 
         <div className="sidebar-wrapper">
-          <div className="sidebar active">
-            <div className="sidebar-item icon">
+          <div className={pc ? "sidebar active" : "sidebar"}>
+            <div onClick={pctoggle} className="sidebar-item icon">
               <GrPersonalComputer color="white" />
             </div>
           </div>
 
-          {/* <div className="sidebar ">
-            <div className="sidebar-item">
-              <GrPersonalComputer className="sidebar-icon" />
+          <div className={face ? "sidebar active" : "sidebar"}>
+            <div onClick={facetoggle} className="sidebar-item icon">
+              <BiBriefcase className="sidebar-icon" />
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
